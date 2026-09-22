ee736555f3bc2e9e2949c966859e0d1e28b2f90b
monacoluxurycars7 <monacoluxurycars7@gmail.com>
2026-09-22 17:30:32 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import Head from 'next/head';
 import emailjs from '@emailjs/browser';
 import { db } from '../lib/firebase';
-import { collection, addDoc, onSnapshot } from 'firebase/firestore';
+import { collection, addDoc, onSnapshot, getDocs } from 'firebase/firestore';
 
 
 

f91d0120872227207a0b8d35c02a208198cf6f09
monacoluxurycars7 <monacoluxurycars7@gmail.com>
2026-09-24 18:53:57 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <link rel="icon" href="/logo.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="apple-touch-icon" href="/fondo admin.png" />
       </Head>
       <Component {...pageProps} />
     </>

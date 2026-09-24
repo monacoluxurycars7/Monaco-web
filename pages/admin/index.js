d49b922e80f86c7a101352a614ed4938354bd79b
monacoluxurycars7 <monacoluxurycars7@gmail.com>
2026-09-23 22:37:08 -0400

    Update index.js

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
-import ContratoModal from './ContratoModal';
+import Link from 'next/link';
+import ContratoModato from '../ContratoModal';
 import { initializeApp, getApps } from 'firebase/app';
 import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
 import { getFirestore, collection, onSnapshot, query, orderBy, doc, updateDoc, deleteDoc } from 'firebase/firestore';
